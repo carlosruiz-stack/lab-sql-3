4d8b4c0e876550ee80ea2ce2b8e6a3801575202e
carlosruiz-stack <122636522+carlosruiz-stack@users.noreply.github.com>
2023-02-21 21:37:45 +0100

    Update new movie db query.sql

--- new movie db query.sql
+++ new movie db query.sql
@@ -1,19 +1,32 @@
 USE sakila;
 
+#1 How many distinct (different) actors' last names are there?
+
 SELECT DISTINCT last_name
 FROM actor;
+
+#2In how many different languages where the films originally produced? (Use the column language_id from the film table)
 
 SELECT DISTINCT language_id
 FROM film;
 
+#3 How many movies were released with "PG-13" rating?
+
 SELECT Count('PG-13')
 FROM film;
+
+#4 Get 10 the longest movies from 2006.
 
 SELECT * FROM film
 WHERE release_year = 2006
 ORDER BY length ASC
 LIMIT 10; 
 
+#5 SELECT rental_id, DATE_FORMAT(rental_date,"%Y-%m-%d") AS first_rental_date, datediff('2005-05-24',DATE_FORMAT(rental_date,"%Y-%m-%d")) AS days_of_dif
+
+FROM rental
+ORDER BY rental_date DESC
+LIMIT 1;
 
 
 
