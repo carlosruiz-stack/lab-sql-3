ccf036739b3b443fe908f04cfbab888f14542d72
carlosruiz-stack <122636522+carlosruiz-stack@users.noreply.github.com>
2023-03-03 10:44:17 +0100

    Update new movie db query.sql
    Added response to questions 6, 7 and 8

--- new movie db query.sql
+++ new movie db query.sql
@@ -22,13 +22,38 @@ WHERE release_year = 2006
 ORDER BY length ASC
 LIMIT 10; 
 
-#5 SELECT rental_id, DATE_FORMAT(rental_date,"%Y-%m-%d") AS first_rental_date, datediff('2005-05-24',DATE_FORMAT(rental_date,"%Y-%m-%d")) AS days_of_dif
+#5 How many days has been the company operating (check DATEDIFF() function)?
 
+SELECT rental_id, DATE_FORMAT(rental_date,"%Y-%m-%d") AS first_rental_date, datediff('2005-05-24',DATE_FORMAT(rental_date,"%Y-%m-%d")) AS days_of_dif
 FROM rental
 ORDER BY rental_date DESC
 LIMIT 1;
 
+#6 Show rental info with additional columns month and weekday. Get 20. 
 
+SELECT rental_id, rental_date, 
+WEEKDAY(rental_date) AS wkday, 
+MONTH(rental_date) AS mo
+FROM rental
+ORDER BY rental_date DESC
+LIMIT 20;
 
+#7 Add an additional column day_type with values 'weekend' and 'workday' depending on the rental day of the week (suggested approach, but with run error)
 
+SELECT rental_id, rental_date, 
+WEEKDAY(rental_date), MONTH(rental_date) AS day_type
+FROM rental
+ORDER BY rental_date DESC
+(CASE 
+	WHEN WEEKDAY(rental_date) BETWEEN 0 AND 4 THEN 'wkday'
+	ELSE 'weekend'
+END); 
 
+#8 How many rentals were in the last month of activity?
+
+This is the query that I implement to get the rentals in the last month of activity, feb. 2006. Last rental date is 2006-02-14 
+
+SELECT COUNT(rental_date)
+FROM rental
+WHERE rental_date = '2006-02-14 15:16:03'
+ORDER BY rental_date DESC; 
